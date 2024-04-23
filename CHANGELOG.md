dc20a957be8380c367ce61f8ff620021f629e78a
JSUCSD <156724636+JSUCSD@users.noreply.github.com>
2024-04-22 18:06:10 -0800

    yay
    yaaay

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+#1 CHANGELOG
+
+## {4/22/2024}
+### Contributors
+Josue Solano    
+
+### Added
+finished the VU meter, added the code, tested it and made some threshold adjustments.
+### Changed
+...
+
+### Removed
+...
+
--- main.py
+++ main.py
@@ -11,27 +11,37 @@ status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
-    board.IO26, # type: ignore
+    board.IO26, #type:ignore
     board.IO47,
-    # do the rest...
+    board.IO33, #type:ignore
+    board.IO34, #type:ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
 
+thresholds = [20000, 25000, 26000, 27000, 28000, 30000, 40000, 45000, 50000, 51000, 52000]
+
 for led in leds:
     led.direction = Direction.OUTPUT
 
-# main loop
 while True:
     volume = microphone.value
 
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    leds_to_light = 0
+    for i, threshold in enumerate(thresholds):
+        if volume >= threshold:  # Check if volume exceeds the threshold
+            leds_to_light = i + 1  
 
-    sleep(1)
+    # Turn on/off LEDs
+    for i, led in enumerate(leds):
+        led.value = i < leds_to_light
 
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    sleep(0.1)
